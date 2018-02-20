7d510e6d84eef53d33e6c12ac117e978c343d64b
Oscar <o.stenqvist@gmail.com>
2018-02-20 16:53:04 +0100

    Lab1

---
+++ 1.5-Datastrukturer.py
@@ -0,0 +1,61 @@
+# Uppgift 1.5.1 I NetProgLab01
+# a = [1,2,3,1,1,1,5,7,2]
+# print(a.count(1)) #Skriver ut hur många 1or som finns i listan
+#
+# a.sort
+# print(a)
+
+# stack = [1, 2, 3, 4]
+# stack.append(8)
+# stack.pop()
+# stack.pop()
+# print(stack)
+
+# que = [1,2,3,4]
+# que.append(8)
+# que.append(9)
+# que.pop(0)
+# que.pop(0)
+# print(que)
+
+# Två olika funktioner som skriver ut samma sak:
+# a = []
+# for x in range(10):
+#     a.append(x*2)
+# print(a)
+#
+# b = [x*2 for x in range(10)]
+# print(b)
+
+# ----------------------------------------------------
+# 1.5.2 TUPLES och SEQUENCES
+# t = 1111, 2222, 3333
+# print(t.count(1111))
+# t.append(4444) # INTE OK OM DET ÄR EN TUPPLE
+# print(t)
+# t.pop() # INTE OK OM DET ÄR EN TUPPLE
+# print(t)
+
+# answ = ["yes", "ja", "japp", "ok"]
+# a = input().lower()
+# if (a in answ):
+#     print("Jahop...")
+# else:
+#     print("Nehep")
+
+# a = set('Hej jag heter Oscar!')
+# print (a)
+
+def f(a):
+    for i in a:
+        print(i, a[i], end=' : ')
+    print()
+
+def func2(it):
+    for i, s in it.items():
+        print(i,s, end=' : ')
+    print()
+
+lista = {"uh": 2323, "ah": 2424, "Oscar": 2525, "Japp":8989, "Nätverk" : 6767}
+f(lista)
+func2(lista)
---
+++ hello.py
@@ -0,0 +1,102 @@
+# Uppgift 4.4 på python.org
+# for n in range(2,10):
+#     for x in range(2,n):
+#         if n % x == 0:
+#             print(n, 'equals', x, '*', n//x)
+#             break
+#     else: # Körs om inte break har körts
+#         print(n, 'is a prime number')
+
+# Uppgift 4.4 på python.org
+# for num in range(2, 10):
+#     if num % 2 == 0:
+#         print("Found an even number", num)
+#         continue # Fortsätter på nästa loop
+#     print("Found a number", num)
+
+# Uppgift 4.5 på python.org
+# while True:
+    # pass # Gör ingenting. Tryck ctrl + c. Pass används istället för {}
+
+# Uppgift 4.6 på python.org
+# def fib(n):
+#     a, b = 0, 1
+#     while a < n:
+#         print(a, end=' ') # end=' ' gör att det inte blir en ny rad utan istället ett mellanslag efter det att den har skrivit ut variabeln
+#         a, b = b, a+b
+#     print()
+#
+# fib(2000) # Funktionsanrop
+
+# Uppgift 1.4.6 I NetProgLab01
+# 1:
+# def hello(n: int):
+#     for i in range(n):
+#         print("Pappegoja", end=' ')
+#     print()
+# # hello(10)
+# # 2:
+# f = hello
+# f(3)
+
+# Uppgift 4.6 på python.org
+# Returnerande Funktion
+# def fib2(n: int):
+#     result = []
+#     a, b = 0, 1
+#     while a < n:
+#         result.append(a)
+#         a, b = b, a+b
+#     return result
+#
+# f100 = fib2(100)
+# print(f100)
+
+# Uppgift 4.7 på python.org
+# def ask_ok(prompt, retries=4, reminder='Please try again'):
+#     while True:
+#         ok = input(prompt)
+#         if ok in ('y', 'ye', 'yes'):
+#             return True
+#         if ok in ('n', 'no', 'nop', 'nope'):
+#             return False
+#         retries = retries - 1
+#         if retries < 0:
+#             raise ValueError('invalid user response')
+#         print(reminder)
+# ask_ok("Do you realy want to quit?!")
+
+# Uppgift 1.4.6 I NetProgLab01
+# 5:
+# djur = ["Hund", "Katt", "Elefant"]
+# def f(arg, ap):
+#     arg.append(ap)
+#     return arg
+#
+# djur = f(djur, "Loppa")
+# print (djur)
+
+# Uppgift 1.4.7 I NetProgLab01
+# Keyword arguments
+# def print_all(*arg, **keyword):
+#     for kw in keyword:
+#         print(kw, ":", keyword[kw])
+#
+# print_all(uh = "ababa", eh = "gross", yh = "hft")
+
+# def print_all2(*arg):
+#     for kw in arg:
+#         print(kw)
+#
+# print_all2("ababa", "gross", "hft")
+
+# Uppgift 4.7.5 på python.org
+# def make_incrementor(n):
+#     return lambda x: x + n
+# f = make_incrementor(42)
+# print(f(0))
+# print(f(1))
+#
+# g = lambda a,b: a+b
+#
+# print(g(3,2))
