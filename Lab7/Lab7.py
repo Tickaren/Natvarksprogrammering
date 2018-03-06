b2af5c9e13bfb8cb978155ce38210503eccdff67
Oscar <o.stenqvist@gmail.com>
2018-03-06 14:53:46 +0100

    7an klar!

--- Lab7/Lab7.py
+++ Lab7/Lab7.py
@@ -1,12 +1,23 @@
 import socket
 import select
 
-port = 60003
-sockL = socket.socket(socke.AF_INET, socket.SOCK_STREAM)
-sockL.bind(("", port))
-sockL.listen(2)
+def broadcast(sock, message):
+    for s in listOfSockets:
+        if s is not server_socket and s is not sock:
+            try:
+                s.send(bytearray(message, "ASCII"))
+            except:
+                s.close()
+                listOfSockets.remove(s)
+                print("Send failed")
 
-listOfSockets = [sockL]
+port = 60003
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(("", port))
+server_socket.listen(10)
+
+listOfSockets = [server_socket]
+listOfSockets.append(server_socket)
 
 print ("Lyssnar på port {}".format(port))
 
@@ -14,20 +25,26 @@ while True:
     tup = select.select(listOfSockets, [], [])
     sock = tup[0][0]
 
-    if sock == sockL:
-        pass
+    if sock == server_socket:
+        Sockclient, addr = server_socket.accept()
+        print ("New connection from:", addr)
         # TODO ny klient ansluter sig.
-        # anropa (Sockclient, addr) = sock.accept() och ta hand om den nya klienten
+        # anropa (Sockclient, addr) = sockL.accept() och ta hand om den nya klienten
         # stoppa in klientens socket i socketlist
+        listOfSockets.append(Sockclient)
+        broadcast(Sockclient, "{} (Connected)".format(addr))
     else:
-        pass
         # Befintlig klient skickar data eller kopplar ner
         data = sock.recv(2048)
         if not data:
-            pass
-            # TODO hantera dedkoppling från sock
+            broadcast(sock, "{} (Nedkopplad)".format(addr))
+            print("klient ({}) är offline".format(addr))
+            sock.close()
+            listOfSockets.remove(sock)
+            # TODO hantera nedkoppling från sock
             # Stäng socketförbindelsen och ta bort sock från listan
         else:
-            pass
+            broadcast(sock, "\r" + "<" + str(sock.getpeername()) + ">" + data.decode("ASCII"))
+            print("<" + str(sock.getpeername()) + ">" + data.decode("ASCII"))
             # TODO data är ett meddelande från klienten
             # skicka detta till alla klienter
